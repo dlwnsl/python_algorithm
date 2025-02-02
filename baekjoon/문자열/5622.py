eae6505d39fae8fed6c33805ec44d931a29e8fbd
JunHee <z1top159@gmail.com>
2025-02-02 21:20:09 +0900

    feat: 5622번 다른 풀이

--- baekjoon/문자열/5622.py
+++ baekjoon/문자열/5622.py
@@ -14,3 +14,13 @@ S = input().strip()
 sum = sum(dial[i] + 1 for i in S)
 
 print(sum)
+
+
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+a = input()
+ret = 0
+for j in range(len(a)):
+    for i in dial:
+        if a[j] in i:
+            ret += dial.index(i)+3
+print(ret)
